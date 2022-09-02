f4d4922cfe56beaabf20da93fae2d5265ae77cf8
SimonOlajec <104787813+SimonOlajec@users.noreply.github.com>
2022-09-02 19:26:00 +0200

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("MOZEM?")
+})
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendString("MOZEM?")
+    radio.sendNumber(2)
 })
 radio.setGroup(1)
